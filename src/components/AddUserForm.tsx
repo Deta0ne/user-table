fe7b91a459c091d45255b48f2bd9d1e9877c993e
Mert Yildiz <cyhaeed.mert@gmail.com>
2023-05-04 14:37:34 +0300

    Form MUI kullanarak oluşturuldu

--- src/components/AddUserForm.tsx
+++ src/components/AddUserForm.tsx
@@ -4,11 +4,16 @@ import {
   Stack,
   TextField,
   FormControl,
+  FormLabel,
   InputLabel,
   Select,
   MenuItem,
+  Button,
+  ToggleButtonGroup,
+  ToggleButton,
 } from "@mui/material";
 import { SelectChangeEvent } from "@mui/material/Select";
+import "../css/AddUserForm.css";
 
 const AddUserForm = () => {
   // Rol State
@@ -17,13 +22,22 @@ const AddUserForm = () => {
     setRole(event.target.value as string);
   };
 
+  // Avatar State
+  const [selectedAvatar, setSelectedAvatar] = React.useState("");
+  const handleAvatarChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newAvatar: string
+  ) => {
+    setSelectedAvatar(newAvatar);
+  };
+
   return (
     <div>
       <Box>
-        <Stack>
-          <TextField />
-          <TextField />
-          <TextField />
+        <Stack spacing={3}>
+          <TextField label="Full Name" variant="outlined" size="small" />
+          <TextField label="Username" variant="outlined" size="small" />
+          <TextField label="Email Address" variant="outlined" size="small" />
           <FormControl>
             <InputLabel>Rol</InputLabel>
             <Select
@@ -39,6 +53,37 @@ const AddUserForm = () => {
             </Select>
           </FormControl>
         </Stack>
+        <Stack>
+          <FormLabel>Select Avatar</FormLabel>
+          <ToggleButtonGroup
+            value={selectedAvatar}
+            exclusive
+            onChange={handleAvatarChange}
+            aria-label="avatar selection"
+          >
+            {Array.from({ length: 6 }, (_, i) => i + 1).map((avatarIndex) => (
+              <ToggleButton
+                key={`Avatar${avatarIndex}`}
+                value={`Avatar${avatarIndex}`}
+                aria-label={`Avatar${avatarIndex}`}
+                className={`avatar-button${
+                  selectedAvatar === `Avatar${avatarIndex}`
+                    ? " avatar-button-selected"
+                    : ""
+                }`}
+              >
+                <img
+                  src={`/src/assets/Avatar${avatarIndex}.png`}
+                  alt={`Avatar${avatarIndex}`}
+                  className="avatar-image"
+                />
+              </ToggleButton>
+            ))}
+          </ToggleButtonGroup>
+        </Stack>
+        <Button type="submit" variant="contained" className="form-button">
+          Create User
+        </Button>
       </Box>
     </div>
   );
